a4911107267a573d78e24b17406dcab0af092d3d
Gabe Chantayan <garnet638@gmail.com>
2024-04-06 15:42:08 -0400

    Account and Locales

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,35 +1,37 @@
 import React, { useEffect } from 'react';
-
 import { useNavigate } from 'react-router-dom';
+import { t } from '../assets/js/locale.js';
+import { greet } from '../assets/js/utils.js';
+import { useCookies } from 'react-cookie';
 
 export default function PageOne(params) {
-	// const [pages, setPages] = React.useState(null);
+	const [username, setUsername] = React.useState(''); // Username
+	const [password, setPassword] = React.useState(''); // Password
+	const [cookies, setCookie] = useCookies(['user']);  // Any cookies
 
-	// const navigate = useNavigate();
-	// // useEffect(() => {
-
-	// //     async function getData() {
-	// //         let ret = await account.getListOfAllPermissiblePages();
-
-	// //         ret.sort((a, b) => a.sort - b.sort);
-
-	// //         setPages(ret);
-	// //     }
-
-	// //     getData();
-
-	// // }, [])
-
-	// const goToLogin = () => {
-	// 	navigate('/login');
-	// };
-
-	// const lll = await api.post(['accounts', 'listAllPermissiblePages'], { 'username': username, 'token': ret.token });
-	// console.log(lll)
+    const [locale, setLocale] = React.useState();
 
 	return (
-		<div>
-			<p>Page 1!</p>
+		<div id='main'>
+			<div class='section'>
+				'<p>{greet()}</p>
+				<p>{t('someText')}</p>
+				<p>{t('someMoreText')}</p>
+				<p>{t('engOnly')}</p>
+				<p>{t('freOnly')}</p>
+				<p>{t('armOnly')}</p>
+			</div>
+
+			<div class='section'>
+
+                <label>
+                    Select a language
+                    <select name="localeSelect" value="locale">
+                        <option value="en-US">English</option>
+                    </select>
+                </label>
+
+            </div>
 		</div>
 	);
 }
